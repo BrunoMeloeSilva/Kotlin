857c3c6dfc6523aa14d4018a22a266169be96f30
brunomeloesilva <brunomeloesilva@gmail.com>
2021-05-27 20:03:30 -0400

    chamando funcao dentro de funcao, e LOOPs

--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -12,6 +12,35 @@ fun main(){
     var varialvelB: String = "Maria"
     println(varialvelB)
 
+    //chamando funcao
+    testaCondicoesIF()
+
+    //Loop N vezes determinada
+    for (i in 1..3) {
+        print(" $i")
+    }
+
+    //Loop N vezes determinada, de trás pra frente
+    println()
+    for (i in 3 downTo 1) {
+        print(" $i")
+    }
+
+    //Loop N vezes determinada, de trás pra frente, pulando de 2 em 2
+    println()
+    for (i in 6 downTo 1 step 2) {
+        print(" $i")
+    }
+
+    //Loop N vezes determinada, pode-se usar: break ou continue
+    println()
+    for (i in 1..3) {
+        print(" $i")
+        break
+    }
+}
+
+fun testaCondicoesIF() {
     //IF..igual ao Java
     var saldo = -1
 
