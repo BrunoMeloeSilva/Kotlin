5b79e78e5139a87f7e8c96ec00d49a23d420f144
brunomeloesilva <brunomeloesilva@gmail.com>
2021-05-30 10:47:44 -0400

    Criando objetos anonimos

--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -4,5 +4,25 @@ fun main() {
     var bruno = Gerente(123, "Bruno Silva", "11122233343", 80000.00)
     println("O funcionário ${bruno.nome} de cpf ${bruno.cpf}, tem o salário ${bruno.salario}")
 
+    //gerando objeto anonimo
+    val obj = object {
+        val nome: String = "Bruno"
+        val idade: Int = 22
 
+        //pode ter comportamento e tudo..
+        fun alteraIdade(idade: Int) = this.idade == idade
+    }
+
+    //gerando objeto anonimo: posso até herdar (E se tiver metodos po implementar, é só cumprir o contrato)
+    val obj2 = object : Funcionario("Bruno", "12345", 100.00) {
+        val idade: Int = 22
+
+        //pode ter comportamento e tudo..
+        fun alteraIdade(idade: Int) = this.idade == idade
+
+        override fun bonificacao(): Double {
+            println("Ola")
+            return 2.0
+        }
+    }
 }
