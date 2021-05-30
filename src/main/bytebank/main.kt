49fa32e1bb07bda18e56f554acad778a41c608e8
brunomeloesilva <brunomeloesilva@gmail.com>
2021-05-30 08:51:42 -0400

    Alias para imports

--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -1,4 +1,12 @@
+import jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java
+
+import Java.lang.String as stringJava
+import kotlin.String as stringKotrlin
+
 fun main() {
     var bruno = Gerente(123, "Bruno Silva", "11122233343", 80000.00)
     println("O funcionário ${bruno.nome} de cpf ${bruno.cpf}, tem o salário ${bruno.salario}")
+    val msg : stringJava = "Bruno"
+    val msg2 : stringKotrlin = "Kotlin"
+
 }
